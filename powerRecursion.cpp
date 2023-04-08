cb69999d9c7f8581c854ce5540a44b6c7d00baf5
Shailendra Singh <75472269+ShailendraSingh16@users.noreply.github.com>
2023-04-08 07:01:55 +0530

    Create powerRecursion.cpp

---
+++ powerRecursion.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int power(int a, int b){
+
+    if(b==0){
+        return 1;
+    }
+
+    if(b==1){
+        return a;
+    }
+
+    int ans=power(a,b/2);
+
+    if(b&1){
+        return a*ans*ans;
+    }
+    return ans*ans;
+}
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+
+    cout<<power(a,b);
+
+    return 0;
+}
