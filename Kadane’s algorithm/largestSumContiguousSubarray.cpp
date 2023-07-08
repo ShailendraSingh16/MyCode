3b83db6b95d23b4dd6a7c37156e927e857da5934
Shailendra Singh <75472269+ShailendraSingh16@users.noreply.github.com>
2023-07-08 10:45:40 +0530

    kadane.cpp

--- Kadane’s algorithm/largestSumContiguousSubarray.cpp
+++ Kadane’s algorithm/largestSumContiguousSubarray.cpp
@@ -1,37 +1,34 @@
-/*
- Kadane’s algorithm 
- TC: O(n)
- SC: O(1)
-*/
-
 #include<bits/stdc++.h>
 using namespace std;
 
-int getSum(int a[], int size)
-{
-    int ans = INT_MIN, currSum = 0;
- 
-    for (int i = 0; i < size; i++) {
-        currSum = currSum + a[i];
-        if (ans < currSum)
-            ans = currSum;
- 
-        if (currSum < 0)
-            currSum = 0;
+int kadaneAlgo(int* arr, int n){
+    int currSum=0;
+    int maxSum=INT_MIN;
+
+    for(int i=0;i<n;i++){
+        currSum+=arr[i];
+
+        if(currSum<0){
+            currSum=0;
+        }
+
+        maxSum=max(maxSum,currSum);
     }
-    return ans;
+    return maxSum;
 }
 
 int main(){
     int n;
     cin>>n;
 
-    int arr[n];
+    int* arr = new int[n];
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
 
-    cout<<getSum(arr,n);
+    cout<<kadaneAlgo(arr,n);
+
+ delete []arr;
 
     return 0;
 }
