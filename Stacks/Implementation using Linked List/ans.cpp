ee390cf7b85e3585519bc264214bf753923466bd
Shailendra Singh <75472269+ShailendraSingh16@users.noreply.github.com>
2023-09-02 09:48:24 +0530

    Update ans.cpp

--- Stacks/Implementation using Linked List/ans.cpp
+++ Stacks/Implementation using Linked List/ans.cpp
@@ -1,71 +1,129 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-class Node{
-    public:
+class Node
+{
+public:
     int data;
-    Node* next;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
 };
 
- Node* newNode(int data){
-    Node* node = new Node();
-    node->data=data;
-    node->next=NULL;
-
-    return node;
- }
-
- int isEmpty(Node* root){
-    return !root;
- }
-
- void push(Node** root, int data){
-    Node* node = newNode(data);
-    node->next=*root;
-    *root=node;
- }
-
- int pop(Node** root){
-
-    if(isEmpty(*root))
-    return INT_MIN;
-
-    Node* temp = *root;
-    *root = (*root)->next;
-    int popped=temp->data;
-    free(temp);
-
-    return popped;
- }
-
- int peek( Node* root)
+class Stack
 {
-    if (isEmpty(root))
-        return INT_MIN;
-    return root->data;
-}
+    Node *top;
 
-int main(){
-
-     Node* root = NULL;
-  
-    push(&root, 10);
-    push(&root, 20);
-    push(&root, 30);
-
-    
-    cout << pop(&root) << " popped from stack\n";
-  
-    cout << "Top element is " << peek(root) << endl;
-      
-    cout <<"Elements present in stack : ";
-     //print all elements in stack :
-    while(!isEmpty(root))
+public:
+    Stack()
     {
-        // print top element in stack
-        cout << peek(root) <<" ";
-        // remove top element in stack
-        pop(&root);
+        top = NULL;
     }
+
+    void push(int data)
+    {
+
+        Node *temp = new Node(data);
+
+        if (!temp)
+        {
+            cout << "stack overflow" << endl;
+            exit(1);
+        }
+
+        temp->data = data;
+        temp->next = top;
+        top = temp;
+    }
+
+    bool isEmpty()
+    {
+
+        return top == NULL;
+    }
+
+    int peek()
+    {
+        if (!isEmpty())
+            return top->data;
+        else
+            exit(1);
+    }
+
+    void pop()
+    {
+        Node *temp;
+
+        if (top == NULL)
+        {
+            cout << "stack underflow" << endl;
+        }
+
+        else
+        {
+            temp = top;
+
+            top = top->next;
+
+            free(temp);
+        }
+    }
+
+    void display()
+    {
+
+        Node *temp;
+
+        if (top == NULL)
+        {
+            cout << "stack underflow";
+            exit(1);
+        }
+        else
+        {
+            temp = top;
+
+            while (temp != NULL)
+            {
+                cout << temp->data;
+
+                temp = temp->next;
+                if (temp != NULL)
+                    cout << " -> ";
+            }
+        }
+    }
+};
+
+int main()
+{
+    Stack s;
+
+    // Push the elements of stack
+    s.push(11);
+    s.push(22);
+    s.push(33);
+    s.push(44);
+
+    // Display stack elements
+    s.display();
+
+    // Print top element of stack
+    cout << "\nTop element is " << s.peek() << endl;
+
+    // Delete top elements of stack
+    s.pop();
+    s.pop();
+
+    // Display stack elements
+    s.display();
+
+    // Print top element of stack
+    cout << "\nTop element is " << s.peek() << endl;
+
     return 0;
 }
